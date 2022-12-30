de1f4557ff8fb9dac83c73a09325c29f6c80ab19
Giovana <giovanaworkfg@gmail>
2022-12-30 12:43:12 -0300

    inclui arquivos topo/produtores

---
+++ src/telas/Home/componentes/Topo.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import { View, Text, Image, StyleSheet} from 'react-native'
+
+import { carregaTopo } from '../../../servicos/carregaDados';
+import logo from '../../../../assets/logo.png';
+
+class Topo extends React.Component {
+    state = {
+        topo:{
+            boasVindas: '',
+            legenda: '',
+        },
+    }
+
+    atualizaTopo(){
+        const retorno = carregaTopo();
+        this.setState({topo: retorno})
+        /*console.log(retorno);*/
+    }
+
+    componentDidMount(){
+        this.atualizaTopo();
+    }
+
+    render(){
+        return <View style={estilos.topo}>
+        <Image source={logo} style={estilos.imagem} />
+        <Text style={estilos.boasVindas}>{this.state.topo.boasVindas}</Text>
+        <Text style={estilos.legenda}>{this.state.topo.legenda}</Text>
+    </View>
+    }
+}
+
+
+const estilos = StyleSheet.create({
+    topo: {
+        backgroundColor: '#F6F6F6',
+        padding: 16,
+    },
+    imagem: {
+        width: 70,
+        height: 28,
+    },
+    boasVindas: {
+        marginTop: 24,
+        fontSize: 26,
+        lineHeight: 42,
+        fontWeight: "bold",
+        color: "#464646"
+    },
+    legenda: {
+        fontSize: 16,
+        lineHeight: 26,
+        color: "#A6A6A6"
+    }
+})
+
+export default Topo;
